244c1c08e1edb5c1cb9e0450dc3c7b53be27faa7
정창우 <55183957+JungChangwoo@users.noreply.github.com>
2022-12-29 11:36:37 +0000

    solve

---
+++ Programmers/DP/SchoolRoad.py
@@ -0,0 +1,13 @@
+# [등굣길]
+def solution(m, n, puddles):
+    dp = [[0] * (m+1) for _ in range(n+1)]
+    puddles = [[y, x] for [x, y] in puddles]
+    dp[1][1] = 1
+    for x in range(1, n+1):
+        for y in range(1, m+1):
+            if x == 1 and y ==1: continue
+            if [x, y] in puddles:
+                dp[x][y] = 0
+            else:
+                dp[x][y] = (dp[x-1][y] + dp[x][y-1]) % 1000000007
+    return dp[n][m]
